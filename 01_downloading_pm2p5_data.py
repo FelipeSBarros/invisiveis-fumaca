22a39ffcc9d2a1fa22db723dd000a1544e7f90ce
Felipe Sodré <felipe.b4rros@gmail.com>
2025-01-02 10:01:30 -0300

    transform scripts into executbales

--- 01_downloading_pm2p5_data.py
+++ 01_downloading_pm2p5_data.py
@@ -3,12 +3,14 @@ import cdsapi  # Cliente para acessar os dados do Copernicus
 from datetime import date  # Para manipulação de datas
 import time as t  # Para controlar os intervalos de tempo entre as requisições
 import calendar  # Para obter o último dia de cada mês
-from dateutil.relativedelta import relativedelta  # Para manipulação de datas (como avançar meses)
+from dateutil.relativedelta import (
+    relativedelta,
+)  # Para manipulação de datas (como avançar meses)
 from pathlib import Path  # Para trabalhar com caminhos de arquivos
 import logging  # Para registrar logs de execução
 
 # Inicialização do cliente do Copernicus
-# importante, ver README para saber mais sobre credenciais de acesso
+# importante, ver README para saber mais sobre credenciais de acesso # todo adicionar instruções no README
 server = cdsapi.Client()
 
 # Definição do caminho onde os dados serão armazenados
@@ -24,8 +26,11 @@ PARAMS = {
         "00:00",  # Horários para os quais os dados serão requisitados
         "12:00",
     ],
-    "area": [  # Região geográfica de interesse (coordenadas geográficas da Amazônia)
-        5.3103480484992946, -74.0264933540878332, -18.0761089985748562, -43.9634603545391229
+    "area": [  # Região geográfica de interesse (coordenadas geográficas da Amazônia Legal
+        5.3103480484992946,
+        -74.0264933540878332,
+        -18.0761089985748562,
+        -43.9634603545391229,
     ],
     "leadtime_hour": [  # Intervalos de previsão (em horas UTC)
         "0",  # No início da previsão
@@ -34,65 +39,76 @@ PARAMS = {
     ],
 }
 
-# Definição das datas de início e fim para o download dos dados
-START_DATE = date(2024, 7, 1)  # Data inicial
-END_DATE = date(2024, 12, 31)  # Data final
 
-# Definição dos horários para as requisições
-times = ["00:00", "12:00"]
+def downloading_pm2p5_data(PARAMS, PATH):
+    logging.basicConfig(level=logging.WARNING)
+    # Definição das datas de início e fim para o download dos dados
+    START_DATE = date(2024, 7, 1)  # Data inicial
+    END_DATE = date(2024, 12, 31)  # Data final
 
-# Inicialização da variável para iteração sobre os meses
-dt = START_DATE
-dt_end = dt.replace(day=calendar.monthrange(dt.year, dt.month)[1])  # Último dia do mês
+    # Definição dos horários para as requisições
+    times = ["00:00", "12:00"]
 
-# Verificação se o diretório de destino já existe; se não, cria-o
-if not PATH.exists():
-    PATH.mkdir(parents=True)
+    # Inicialização da variável para iteração sobre os meses
+    dt = START_DATE
 
-# Loop principal para iterar sobre os meses do período definido
-while dt < END_DATE:
-    params = PARAMS.copy()  # Copia os parâmetros iniciais para cada iteração
-    dt_end = dt.replace(day=calendar.monthrange(dt.year, dt.month)[1])  # Último dia do mês atual
-    params[
-        "date"] = f'{dt.strftime("%Y-%m-%d")}/{dt_end.strftime("%Y-%m-%d")}'  # Define o intervalo de datas para o mês
+    # Verificação se o diretório de destino já existe; se não, cria-o
+    if not PATH.exists():
+        PATH.mkdir(parents=True)
 
-    # Loop sobre os horários definidos para cada dia
-    for time in times:
-        # Atualiza o parâmetro "time" com o valor atual do loop
-        params["time"] = time
+    # Loop principal para iterar sobre os meses do período definido
+    while dt < END_DATE:
+        params = PARAMS.copy()  # Copia os parâmetros iniciais para cada iteração
+        dt_end = dt.replace(
+            day=calendar.monthrange(dt.year, dt.month)[1]
+        )  # Último dia do mês em processamento
+        params[
+            "date"
+        ] = f'{dt.strftime("%Y-%m-%d")}/{dt_end.strftime("%Y-%m-%d")}'  # Define o intervalo de datas para o mês
 
-        # Definir o intervalo de previsão de acordo com o horário
-        if time == "00:00":  # Para a hora "00:00", solicita previsões para os passos 6 e 9 horas
-            params["leadtime_hour"] = [6, 9]
-        else:  # Para a hora "12:00", solicita previsões para os passos "0/3/6/9/12/15"
-            params["leadtime_hour"] = [0, 3, 6, 9, 12, 15]
+        # Loop sobre os horários definidos para cada dia
+        for time in times:
+            # Atualiza o parâmetro "time" com o valor atual do loop
+            params["time"] = time
 
-        # Monta o nome do arquivo baseado nos parâmetros da requisição
-        file_name = Path(
-            f"{PATH}/cams_{params['date'].replace('/', '-')}_{params['time']}_{'-'.join(list(map(str, params['leadtime_hour'])))}.netcdf_zip"
-        )
+            # Definir o intervalo de previsão de acordo com o horário do modelo
+            if (
+                time == "00:00"
+            ):  # Para a hora "00:00", solicita previsões para os passos 6 e 9 horas
+                params["leadtime_hour"] = [6, 9]
+            else:  # Para a hora "12:00", solicita previsões para os passos "0/3/6/9/12/15"
+                params["leadtime_hour"] = [0, 3, 6, 9, 12, 15]
 
-        # Verifica se o arquivo já existe. Se existir, pula a requisição para evitar duplicação
-        if file_name.exists():
-            logging.warning(f"File {file_name} already exists. Skipping it.")
-        else:
-            logging.warning(f'Retriving {time} for {params["date"]}')
-
-            # Realiza a requisição de dados ao servidor do Copernicus
-            server.retrieve(
-                "cams-global-atmospheric-composition-forecasts",  # Nome do produto no Copernicus
-                params,  # Parâmetros para a requisição
-                file_name,  # Caminho para salvar o arquivo
+            # Monta o nome do arquivo baseado nos parâmetros da requisição
+            file_name = Path(
+                f"{PATH}/cams_{params['date'].replace('/', '-')}_{params['time']}_{'-'.join(list(map(str, params['leadtime_hour'])))}.netcdf_zip"
             )
 
-            # Pausa por 10 segundos para evitar sobrecarga do servidor
-            t.sleep(10)
+            # Verifica se o arquivo já existe. Se existir, pula a requisição para evitar duplicação
+            if file_name.exists():
+                logging.warning(f"File {file_name} already exists. Skipping it.")
+            else:
+                logging.warning(f'Retriving {time} for {params["date"]}')
 
-            # Log de confirmação após a requisição
-            logging.warning(f'Done {time} for {params["date"]}')
+                # Realiza a requisição de dados ao servidor do Copernicus
+                server.retrieve(
+                    "cams-global-atmospheric-composition-forecasts",  # Nome do produto no Copernicus
+                    params,  # Parâmetros para a requisição
+                    file_name,  # Caminho para salvar o arquivo
+                )
 
-    # Log de confirmação ao final de cada mês
-    logging.warning(f"Done {dt}")
+                # Pausa por 10 segundos para evitar sobrecarga do servidor
+                t.sleep(10)
 
-    # Avança para o próximo mês
-    dt += relativedelta(months=1)
+                # Log de confirmação após a requisição
+                logging.warning(f'Done {time} for {params["date"]}')
+
+        # Log de confirmação ao final de cada mês
+        logging.warning(f"Done {dt}")
+
+        # Avança para o próximo mês
+        dt += relativedelta(months=1)
+
+
+if __name__ == "__main__":
+    downloading_pm2p5_data(PARAMS, PATH)
--- 02_unzip_pm2p5_data.py
+++ 02_unzip_pm2p5_data.py
@@ -4,6 +4,7 @@ import os  # Para manipulação de arquivos e diretórios
 import zipfile  # Para descompactar arquivos zip
 from datetime import datetime  # Para trabalhar com datas
 from pathlib import Path  # Para manipulação de caminhos de arquivos
+import logging  # Para registrar logs de execução
 
 # Para manipulação de datas de forma avançada, como adicionar meses
 from dateutil.relativedelta import relativedelta
@@ -12,45 +13,67 @@ from dateutil.relativedelta import relativedelta
 start_date = datetime(2024, 7, 1).date()  # Data inicial
 end_date = datetime(2024, 12, 31).date()  # Data final
 
-# Inicialização da variável para iteração sobre as datas
-dt = start_date
 
-# Caminho para onde os arquivos descompactados serão armazenados
-dest_path = Path("./Data/Raw/CAMS_AMZ/unzipped")
+def unzip_pm2p5_data(start_date, end_date):
+    # Inicialização da variável para iteração sobre as datas
+    dt = start_date
 
-# Verificação se o diretório de destino existe; se não, cria-o
-if not dest_path.exists():
-    dest_path.mkdir(parents=True)
+    # Caminho para onde os arquivos descompactados serão armazenados
+    dest_path = Path("./Data/Raw/CAMS_AMZ/unzipped")
 
-# Loop principal para iterar sobre as datas dentro do intervalo
-while dt < end_date:
-    # Utiliza o glob para encontrar arquivos com o nome que começa com 'cams_' e a data do mês atual
-    files = glob.glob(f"./Data/Raw/CAMS_AMZ/cams_{dt}*")
+    # Verificação se o diretório de destino existe;
+    if not dest_path.exists():
+        logging.warning(f"Creating {dest_path} folder.")
+        dest_path.mkdir(parents=True)
 
-    # Cria um diretório específico para o mês atual dentro do diretório de destino
-    intermediate_path = Path.joinpath(dest_path, str(dt))
+    # Loop principal para iterar sobre as datas dentro do intervalo
+    while dt < end_date:
+        # Utiliza o glob para encontrar arquivos com o nome que começa com 'cams_' e a data do mês atual
+        files = glob.glob(f"./Data/Raw/CAMS_AMZ/cams_{dt}*")
 
-    # Para cada arquivo encontrado para o mês atual, realiza a extração
-    for file in files:
-        # Converte o caminho do arquivo para um objeto Path
-        file = Path(file)
+        # Cria um diretório específico para o mês atual dentro do diretório de destino
+        intermediate_path = Path.joinpath(dest_path, str(dt))
 
-        # Extrai informações sobre o horário do modelo e o passo de previsão a partir do nome do arquivo
-        model_time = file.name.split("_")[2]  # Exemplo: 00:00, 12:00
-        step = file.name.split("_")[3].split(".")[0]  # Exemplo: 6, 12
+        if intermediate_path.exists():
+            logging.warning(
+                f"{intermediate_path} already exists. Checking if files are already extracted."
+            )
+            # Caso os arquivos já tenham sido extraídos, avança para o próximo mês
+            if len(list(intermediate_path.glob("*.nc"))) == 2:
+                logging.warning(f"Month {dt} already extracted.")
+                dt += relativedelta(months=1)
+                continue
+            continue
 
-        # Abre o arquivo zip para descompactação
-        with zipfile.ZipFile(file, "r") as zip_ref:
-            # Extrai todos os arquivos do zip para o diretório do mês correspondente
-            zip_ref.extractall(intermediate_path)
+        # Para cada arquivo encontrado para o mês atual, realiza a extração
+        for file in files:
+            # Converte o caminho do arquivo para um objeto Path
+            file = Path(file)
 
-        # Renomeia o arquivo extraído para um formato mais organizado
-        # Localiza o arquivo .nc extraído (geralmente é o primeiro arquivo que começa com 'data')
-        os.rename(
-            glob.glob(f"{intermediate_path}/data*.nc")[0],
-            # Renomeia o arquivo extraído com base na data, passo e horário do modelo
-            f"{intermediate_path}/{dt}_{step}_{model_time}.nc",
-        )
+            # Extrai informações sobre o horário do modelo e o passo de previsão a partir do nome do arquivo
+            model_time = file.name.split("_")[2]  # Exemplo: 00:00, 12:00
+            step = file.name.split("_")[3].split(".")[0]  # Exemplo: 6, 12
 
-    # Avança para o próximo mês
-    dt += relativedelta(months=1)
+            # Abre o arquivo zip para descompactação
+            logging.warning(f"Unzipping {file.name}")
+            with zipfile.ZipFile(file, "r") as zip_ref:
+                # Extrai todos os arquivos do zip para o diretório do mês correspondente
+                zip_ref.extractall(intermediate_path)
+
+            # Renomeia o arquivo extraído para um formato mais organizado
+            # Localiza o arquivo .nc extraído (geralmente é o primeiro arquivo que começa com 'data')
+            logging.warning("Renaming extracted file.")
+            os.rename(
+                glob.glob(f"{intermediate_path}/data*.nc")[0],
+                # Renomeia o arquivo extraído com base na data, passo e horário do modelo
+                f"{intermediate_path}/{dt}_{step}_{model_time}.nc",
+            )
+            logging.warning(f"Month {dt} extracted.")
+
+        # Avança para o próximo mês
+        dt += relativedelta(months=1)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.WARNING)
+    unzip_pm2p5_data(start_date, end_date)
