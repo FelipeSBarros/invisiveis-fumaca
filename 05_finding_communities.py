5389db7fedd243494c43ddff1d1f45ebca15c3e1
Felipe Sodré <felipe.b4rros@gmail.com>
2025-01-21 15:05:58 -0300

    update 05_finding_communities.py adding process to a list of variables of interest

--- 05_finding_communities.py
+++ 05_finding_communities.py
@@ -1,36 +1,40 @@
 import geopandas as gpd
+import logging
 
 
 def filter_q90(
     gpkg_path="./Data/Processed/results.gpkg",
     territory="Localidades_Indigenas_pm2p5",
-    variable="media_temporada",
+    variables=["media_temporada", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
     q=0.9,
 ):
+    logging.warning(f"Finding Quantile {q*10} for {territory} GeoDataFrame.")
     territory_gdf = gpd.read_file(gpkg_path, layer=territory)
 
-    q90 = territory_gdf[variable].quantile(q)
-    territory_gdf = territory_gdf[territory_gdf[variable] > q90]
-    territory_gdf.to_file(gpkg_path, layer=f"{territory}_10%_afetadas", driver="GPKG")
-    return
+    for variable in variables:
+        if variable not in territory_gdf.columns:
+            raise ValueError(f"{variable} not found in {territory} GeoDataFrame.")
+        q90 = territory_gdf[variable].quantile(q)
+        filtered_territory_gdf = territory_gdf[territory_gdf[variable] > q90]
+        filtered_territory_gdf.to_file(
+            gpkg_path, layer=f"{territory}_10%_afetadas_{variable}", driver="GPKG"
+        )
+        logging.warning(f"{territory}_10%_afetadas_{variable} saved in {gpkg_path}")
 
 
 if __name__ == "__main__":
     filter_q90(
         gpkg_path="./Data/Processed/results.gpkg",
         territory="Localidades_Indigenas_pm2p5",
-        variable="media_temporada",
         q=0.9,
     )
     filter_q90(
         gpkg_path="./Data/Processed/results.gpkg",
         territory="Localidades_Quilombolas_pm2p5",
-        variable="media_temporada",
         q=0.9,
     )
     filter_q90(
         gpkg_path="./Data/Processed/results.gpkg",
         territory="Favelas_Comunidades_Urbanas_Centroide_pm2p5",
-        variable="media_temporada",
         q=0.9,
     )
